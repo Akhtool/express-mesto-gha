537d067edd8eaa58a6580352de0d709add714323
Akhtool <Zelimhan1997@mail.ru>
2023-06-18 00:44:04 +0300

    fix: исправить кодстайл

--- routes/cards.js
+++ routes/cards.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const { getCards, deleteCard, createCard, addLikeCard, deleteLikeCard } = require('../controllers/cards');
+const {
+  getCards, deleteCard, createCard, addLikeCard, deleteLikeCard,
+} = require('../controllers/cards');
 
 router.get('/', getCards);
 router.delete('/:cardId', deleteCard);
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const { getUsers, getUser, createUser, updateUser, updateAvatar } = require('../controllers/users');
+const {
+  getUsers, getUser, createUser, updateUser, updateAvatar,
+} = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:userId', getUser);
