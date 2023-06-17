753687657eae83f957030cbbc0d29f198e0cfa8a
Akhtool <Zelimhan1997@mail.ru>
2023-06-17 18:47:07 +0300

    feat: подключиться к mongodb

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 const app = express();
 
