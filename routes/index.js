d921db6ba004295381f270c7b4c5afe2e8418137
Akhtool <Zelimhan1997@mail.ru>
2023-06-21 15:13:28 +0300

    feat: создать роут для логина и регистрации

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const { ERROR_CODE_NOT_FOUND } = require('../utils/constants');
 const cardsRouter = require('./cards');
 const usersRouter = require('./users');
 
+const { login, createUser } = require('../controllers/users');
 const auth = require('../middlewares/auth');
+
+router.post('/signin', login);
+router.post('/signup', createUser);
 
 router.use(auth);
 router.use('/users', usersRouter);
