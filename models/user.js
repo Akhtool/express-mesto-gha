ace4dbdc04e8752f738acf9564ca5985273b6c53
Akhtool <Zelimhan1997@mail.ru>
2023-06-21 14:20:38 +0300

    fix: исправить код в модели user

--- models/user.js
+++ models/user.js
@@ -18,14 +18,14 @@ const userSchema = new mongoose.Schema({
   },
   name: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
     maxlength: 30,
     default: 'Исследователь',
