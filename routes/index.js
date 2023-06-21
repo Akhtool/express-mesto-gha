3c847b2ba15bd087b2a3e984621fb86bfcba30f3
Akhtool <Zelimhan1997@mail.ru>
2023-06-21 15:12:28 +0300

    feat: сделать мидлвэр для авторизации

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const { ERROR_CODE_NOT_FOUND } = require('../utils/constants');
 const cardsRouter = require('./cards');
 const usersRouter = require('./users');
 
+const auth = require('../middlewares/auth');
+
+router.use(auth);
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
 router.use('/*', (req, res) => {
